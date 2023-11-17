17c406fb8f0e75a9a6c47594c4c1a8cb43dfa2c3
Mohit Gujarati <mohitgujarati11@gmail.com>
2023-11-17 11:29:04 +0530

    Databinding-and-LiveData-With ViewModel

--- app/src/main/java/com/example/mvvmarc/MainActivity.kt
+++ app/src/main/java/com/example/mvvmarc/MainActivity.kt
@@ -3,50 +3,55 @@ package com.example.mvvmarc
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmarc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    lateinit var mainviewmodel: mainViewModel
+    private lateinit var mainViewModel: MainViewModel
+
     companion object {
-        const val LOGTAG = "LifeCycle"
+        const val LOGTAG = "MainActivityLog"
 
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        val observer = Observer()
-        lifecycle.addObserver(observer)
-        mainviewmodel=ViewModelProvider(this,mainViewModelFactory(0)).get(mainViewModel::class.java)
 
-        mainviewmodel.numberLiveData.observe(this, Observer{
-            binding.textView1.text=it
-        })
+        // Use DataBindingUtil.setContentView to set up data binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // Set the lifecycle owner for observing LiveData
+        binding.lifecycleOwner = this
+        val lifecycleobserver = Observer()
+        lifecycle.addObserver(lifecycleobserver)
 
-        Log.d(LOGTAG, "Activity - onCreate")
 
-        setText()
+        //Adding observer to keep eye on data so as soon as it changes the data here
+        val observer = Observer<String> {
+            binding.textView1.text = it
+        }
 
+        // Initialize ViewModel using ViewModelProvider
+        mainViewModel = ViewModelProvider(this, MainViewModelFactory(0)).get(MainViewModel::class.java)
+
+        // Observe the EOTextLiveData in the ViewModel
+        mainViewModel.EOTextLiveData.observe(this, observer)
+
+        // Set ViewModel to the binding
+        binding.viewModelData = mainViewModel
+
+        // Set up click listener for the button
         binding.btnIncrement.setOnClickListener {
-            increment()
+            mainViewModel.increment()
             setText()
         }
 
-
+        setText()
     }
 
     private fun setText() {
-        binding.textView.text=mainviewmodel.count.toString()
+        binding.textView.text = mainViewModel.count.toString()
     }
-
-    private fun increment(){
-        mainviewmodel.increment()
-    }
-
-
 }
